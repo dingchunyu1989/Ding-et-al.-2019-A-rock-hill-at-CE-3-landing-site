8564f7218c5efb2b495e585b3d07a3e4097e277b
Chunyu Ding <baci.dingchunyu@gmail.com>
2019-11-27 15:02:14 +0800

    Add files via upload

---
+++ image_format.m
@@ -0,0 +1,6 @@
+function image_format
+set(gca,'FontName','Times New Roman','FontSize',12);
+set(gca,'xminortick','on');%style 5
+set(gca,'yminortick','on');%style 5
+set(gca,'zminortick','on');%style 5
+set(gca,'tickdir','out');%style 7
